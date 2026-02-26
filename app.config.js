b963e9fa81c67ed2c6d07c3b89aeddd3728292f4
ibro1 <abubakaribro1@gmail.com>
2026-02-26 05:22:16 +0100

    fixed firebase notification [run preview ci]

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
         name: "KarRide Driver",
         slug: "karride-driver",
         owner: "netlinko",
-        version: "1.0.0",
+        version: "1.0.1",
         orientation: "portrait",
         icon: "./assets/icons/icon.png",
         scheme: "ng.karride.driver",
